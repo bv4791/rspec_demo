67522d1551ccddd4b1cb64782d0128ba11ed0314
bv4791 <bv4791@gmail.com>
2021-03-13 18:28:07 +0200

    Add hero entity

--- hero.rb
+++ hero.rb
@@ -1,2 +1,20 @@
 class Hero
-end
+       def initialize(name, health=100)
+        @name = name.capitalize
+        @health = health
+      end
+
+      attr_reader :name
+
+      def power_up
+        @health += 10
+      end
+
+      def power_down
+        @health -= 10
+      end
+
+      def hero_info
+        "#{@name} has #{@health} health"
+      end
+ end
